efa0605d9e82e46d66dc6f652e3c7c6dba7506e9
Akshay Viswanathan <akshay.viswanathan86@gmail.com>
2014-04-13 06:21:52 -0500

    Cleaning up and adding Javadoc on old code

--- src/soulpatch/com/utilities/fileextensions/MyFile.java
+++ src/soulpatch/com/utilities/fileextensions/MyFile.java
@@ -3,7 +3,7 @@ package soulpatch.com.utilities.fileextensions;
 import java.io.File;
 
 /**
- * Extends the java.io.File class to implement file and directory search
+ * Extends the java.io.File class to implement new methods.
  * functionalities.
  * 
  * @author Akshay Viswanathan
@@ -12,29 +12,20 @@ import java.io.File;
 public class MyFile extends File {
 
 	private static final long serialVersionUID = 1L;
-	File file = null;
-	long fileSize = 0;
-	MyFile[] fileList = null;
-	boolean isSameName = false;
-	boolean isSimilarName = false; // True if the names have any words same.
-	boolean isSameSize = false;
-	boolean isDuplicate = false;
-	String directory = null;
-	String extension = null;
-
+	
 	public MyFile(String pathname) {
 		super(pathname);
 	}
 
 	/**
-	 * True if the names are exact match.
+	 * True if the names without extensions are an exact match.
 	 * 
 	 * @param myFile
 	 * @return
 	 */
 	public boolean isSameName(MyFile myFile) {
-		String name1 = fileNameWOExtension(getName()); // Extract filenames without extension and then compare the names.
-		String name2 = fileNameWOExtension(getName());
+		String name1 = fileNameWOExtension(getName());
+		String name2 = fileNameWOExtension(myFile.getName());
 		if (name1 != null && name2 != null)
 			if (name1.equalsIgnoreCase(name2))
 				return true;
@@ -42,7 +33,7 @@ public class MyFile extends File {
 	}
 
 	/**
-	 * 
+	 * Checks the file extension to see if the files are of the same type
 	 * @param myFile
 	 * @return
 	 */
@@ -53,7 +44,7 @@ public class MyFile extends File {
 	}
 
 	/**
-	 * True if the sizes of the files are exactly same. (Will later modify to
+	 * True if the sizes of the files are exactly same. (Will be later modified to
 	 * have a range)
 	 * 
 	 * @param myFile
@@ -66,41 +57,19 @@ public class MyFile extends File {
 	}
 
 	/**
-	 * True if the above three boolean variables for each file combination is
-	 * true.
+	 * True if the files have the same name, type and size
 	 * 
 	 * @param myFile2
 	 * @return
 	 */
 	public boolean isDuplicate(MyFile myFile) {
-		if (isSameName(myFile) && isSameSize(myFile))
+		if (isSameName(myFile) && isSameSize(myFile) && isSameType(myFile))
 			return true;
 		return false;
 	}
 
 	/**
-	 * 
-	 * @param myfile1
-	 * @return
-	 */
-	public boolean equals(MyFile myfile1) {
-		String file1 = getParent();
-		String file2 = myfile1.getParent();
-
-		if (file1.equalsIgnoreCase(file2)) {
-			return true;
-		}
-		return false;
-	}
-
-	/**
-	 * 
-	 * The file extension is generally the bit after the last '.' in the full
-	 * file name. e.g Beatles.Abbey.Road.mp3 has the extension mp3 and the file
-	 * name "Beatles.Abbey.Road". The latter is what we want to use to compare
-	 * the names of the files. After finding similar named files, we can compare
-	 * the extension and ascertain if the files are actually duplicates or just
-	 * named same. So album.mp3 , album.jpg both will come up in this search.
+	 * Returns the file type extension of the file.
 	 * 
 	 * @param fileName - Full file name
 	 * @return extension - File extension
@@ -117,11 +86,10 @@ public class MyFile extends File {
 	}
 
 	/**
-	 * Takes the output of the File.getName() and returns only the file name
+	 * Returns the filename without the file type extension
 	 * without the extension. "Flute.mp3" will give "Flute"
 	 * 
-	 * @param fileName
-	 *            - Full file name with extension
+	 * @param fileName - Full file name with extension
 	 * @return fileNameOnly - File name without extension
 	 */
 	public static String fileNameWOExtension(String fileName) {
@@ -134,6 +102,7 @@ public class MyFile extends File {
 		return fileNameOnly;
 	}
 	/**
+	 * Returns true if the file is of extension type
 	 * 
 	 * @param extension
 	 * @return
@@ -143,35 +112,12 @@ public class MyFile extends File {
 	}
 
 	/**
-	 * 
-	 * @param directory
-	 */
-	public void setDirectory(String directory) {
-		this.directory = directory;
-	}
-
-	/**
-	 * 
-	 * @param extension
-	 */
-	public void setFileExtension(String extension) {
-		this.extension = extension;
-	}
-
-	/**
+	 * Gets the size of the file in KBs (File size divided by 1024)
 	 * 
 	 * @return
 	 */
 	public double getFileSize() {
 		double size = length() / 1024; // FileSize in kilobytes
 		return size;
-	}
-
-	/**
-	 * 
-	 * @param fileSize
-	 */
-	public void setFileSize(long fileSize) {
-		this.fileSize = fileSize;
 	}
 }
